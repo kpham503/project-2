2cdc8f4499726c61c7b19b7428580ae7f3254444
Kevin Pham <kevinpham@Kevins-MacBook-Pro-3.local>
2020-04-23 23:33:25 -0700

    add images.

--- client/javascripts/functions.js
+++ client/javascripts/functions.js
@@ -1,16 +1,14 @@
 // jshint esversion: 6
 
-let containsDuplicates = s => {
-  let cArr = [0, 0, 0, 0],
-    code;
-  for (let i in cArr) {
-    code = s[i].toUpperCase().charCodeAt(0) - 65;
-    //console.log(code);
-    if (cArr[code] > 0) {
-      return true;
-    } else {
-      cArr[i]++;
-    }
-  }
-  return false;
-};
+//3-A
+
+
+
+//3-B
+
+
+
+
+
+
+//3-C
--- client/javascripts/gallery.js
+++ client/javascripts/gallery.js
@@ -1,15 +1,19 @@
 // jshint esversion: 6
 
-let main = function(){
+let controller = function(){
 
     let prefixURL = "http://api.flickr.com/services/feeds/photos_public.gne?tags=";
     let suffixURL = "&format=json&jsoncallback=?";
     //get value entered by user from textbox
-    let flickrTag = $("input").???();
+    let flickrTag = document.querySelector("input[type=text]").value;
+    //let flickrTag = $("input").???()
+    console.log(flickrTag);
     let requestURL = prefixURL + flickrTag + suffixURL;
+    console.log(requestURL);
+    document.querySelector(".photos").innerHTML = "";
 
     //clear old photos
-    $(".photos").???("");
+    //$(".photos").???("");
 
   $.getJSON(requestURL, function(flickrResponse) {
     flickrResponse.items.forEach(function(item, index) {
@@ -24,7 +28,7 @@ let main = function(){
         // set the attribute to the url
         // contained in the response
         $img.attr("src", item.media.m);
-        $img.attr("width", "100");
+        $img.attr("width", "150");
 
         // attach the img tag to the main
         // photos element and then fade it in
@@ -36,5 +40,15 @@ let main = function(){
   });
 };
 
+//$(document).ready(controller);
 
-$(document).ready(main);
+//$(document).ready(main);
+
+//Register the controller after the DOM is complete
+window.addEventListener("load", () => {
+  //select the button
+  let button = document.querySelector("button");
+
+  //register the click handler for the button
+  button.addEventListener("click", controller);
+});
