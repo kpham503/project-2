2024568e3dd9bb0e4478e82a518d4708c3c171e1
Kevin Pham <kevinpham@Kevins-MacBook-Pro-3.local>
2020-04-24 20:16:09 -0700

    add functions to functions.js

--- client/javascripts/functions.js
+++ client/javascripts/functions.js
@@ -1,14 +1,33 @@
 // jshint esversion: 6
 
 //3-A
-
-
+let revWords1 = (str) => {
+  var reverse1 = "";
+  var reverseWord1 = str.split("");
+  for (var i = 0; i < reverseWord1.length; i--){
+    reverse1 += reverseWord1[i];
+  }
+  return reverse1;
+};
 
 //3-B
 
-
-
-
-
+let revWords2 = (str) => {
+  var reverse2 = "";
+  var arr = str.split("");
+  arr.reverse().forEach(function(reverse2) {
+  console.log(reverse2);
+});
+};
 
 //3-C
+
+let revWords3 = (str) => {
+  var reverse3 = "";
+  var reverseWord3 = str.split("");
+  var reverseWords3 = reverseWord3.reverse();
+  for (var x of reverseWords3){
+    reverse3 = reverse3 + x;
+  }
+  return reverse3;
+};
